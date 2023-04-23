de226f2917fbdd6e71234c1cab8b79b188eaaed8
andrew415 <awhite@integratedauctionsolutions.com>
2023-04-22 21:07:38 -0400

    add make vectors script

--- scripts/make_vectors.py
+++ scripts/make_vectors.py
@@ -1,4 +1,7 @@
 import numpy as np
+from termcolor import colored
+import argparse
+import re
 
 def char_to_index(char, alphabet="abcdefghijklmnopqrstuvwxyz"):
     return alphabet.index(char.lower())
@@ -17,9 +20,43 @@ def word_to_vectors(word, alphabet="abcdefghijklmnopqrstuvwxyz"):
             word_vectors.append(vector)
     return np.array(word_vectors)
 
+def print_colored_vectors(word_vectors):
+    for row in word_vectors:
+        colored_row = ""
+        for val in row:
+            if val == 1:
+                colored_row += colored(str(int(val)), "green")
+            else:
+                colored_row += colored(str(int(val)), "red")
+            colored_row += " "
+        print(colored_row)
+
+def main(input_file=None, word=None):
+    if input_file:
+        with open(input_file, 'r') as file:
+            text = file.read()
+            words = re.findall(r'\b\w+\b', text)
+            for word in words:
+                word_vectors = word_to_vectors(word)
+                print(f"Word: {word}")
+                print("Vectors:")
+                print_colored_vectors(word_vectors)
+                print()
+    elif word:
+        word_vectors = word_to_vectors(word)
+        print(f"Word: {word}")
+        print("Vectors:")
+        print_colored_vectors(word_vectors)
+        print()
+
 if __name__ == "__main__":
-    word = "hello"
-    word_vectors = word_to_vectors(word)
-    print(f"Word: {word}")
-    print("Vectors:")
-    print(word_vectors)
+    parser = argparse.ArgumentParser(description='Convert words to one-hot encoded vectors.')
+    parser.add_argument('-f', '--file', type=str, help='Path to the input file')
+    parser.add_argument('-w', '--word', type=str, help='Convert a single word into vector data.')
+    args = parser.parse_args()
+
+    if args.file is None and args.word is None:
+        parser.print_help()
+        exit()
+
+    main(input_file=args.file, word=args.word)
